095defa8e7917d022a52779e5b80da261c879d19
Sergey <benifeks72@gmail.com>
2024-01-31 19:49:23 +0200

    for review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { router } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BlackJackComponent } from './components/black-jack/black-jack.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { HorseComponent } from './components/horse/horse.component';
+import { MakeUpTableComponent } from './components/make-up-table/make-up-table.component';
+import { FieldComponent } from './components/horse/field/field.component';
+import { SideBarComponent } from './components/horse/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackJackComponent,
+    HeaderComponent,
+    HomeComponent,
+    HorseComponent,
+    MakeUpTableComponent,
+    FieldComponent,
+    SideBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, router],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
